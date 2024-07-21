da54a97549bf86a837a5b5d6c8f64834cc95f091
Jeph Mari Daligdig <daligdigjeph09@gmail.com>
2024-07-21 13:47:36 +0800

    adding more challenges

---
+++ JavaScriptStringManipulation/js/challenge.js
@@ -0,0 +1,13 @@
+function photoName() {
+  //prompt person to enter name 
+  var name = prompt("Please enter your name");
+  //remove additional whitespaces
+  name = name.trim();
+  //replace the space between names with underscore
+  name = name.replace(" ", "_");
+  //change name to all lowercase
+  name = name.toLowerCase();
+  alert("Your photo name is " + name + ".jpg");
+}
+
+photoName();
--- dynamic_select_menus_starter/script.js
+++ dynamic_select_menus_starter/script.js
@@ -10,26 +10,25 @@ const breedsSelectElement = document.querySelector("#breeds");
 const optionElements = document.querySelectorAll('option[data-breed]');
 // 4: add an event listener on the animals select element that listens for the "change" event
 animalsSelectElement.addEventListener('change', (e) => {
-  optionElements.forEach((option) => {
-    console.log(animalsSelectElement.value);
-    if (animalsSelectElement.value.includes(option.attributes['data-breed'].value)) {
-
+  //   5: start a loop to iterate over each breed option
+  for (let i = 0; i < optionElements.length; i++) {
+    //     6: check if the animals select value is not equal to the current breed option's data-breed attribute value
+    if (animalsSelectElement.value !== optionElements[i].getAttribute("data-breed")) {
+      //       7: if they are not equal, hide the current breed option
+      optionElements[i].hidden = true;
+      optionElements[i].disabled = true;
     } else {
-      option.attributes['data-breed']
+      //       8: if they are equal, do not hide (or show) the current breed option
+      optionElements[i].hidden = false;
+      optionElements[i].disabled = false;
     }
-  })
-})
-//   5: start a loop to iterate over each breed option
-
-//     6: check if the animals select value is not equal to the current breed option's data-breed attribute value
-
-//       7: if they are not equal, hide the current breed option
-
-//       8: if they are equal, do not hide (or show) the current breed option
-
-//   9. check if the breeds select value is NOT equal to an empty string
-
-//     10. if it's not an empty string, set the breeds select value to the second option's value
+    //   9. check if the breeds select value is NOT equal to an empty string
+    if (breedsSelectElement.value !== "") {
+      //     10. if it's not an empty string, set the breeds select value to the second option's value
+      breedsSelectElement.value = "reselect";
+    }
+  }
+});
 
 // TESTING TIP: Select an animal, select a matching breed, then change
 // the animal again, see if the breed also changes to "Please choose again"
