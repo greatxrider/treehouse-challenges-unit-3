1921d52e25f872f9ee7e56283b61cf611a2f868a
Jeph Mari Daligdig <daligdigjeph09@gmail.com>
2024-07-21 14:39:44 +0800

    finish checkbox dom manipulation

--- practice-dom-manipulation-checkboxes/js/app.js
+++ practice-dom-manipulation-checkboxes/js/app.js
@@ -1,15 +1,31 @@
 //1. Target the form element in the DOM.
-  
-//2. Attach an event listener to the form element to listen for any change events.
+const formElement = document.querySelector('form');
 
-//3. Inside the event listener:
-//  - Target the checkbox that triggered the change event.
-//  - Get the combat style of the triggering checkbox from its data-combat-style attribute.
-//  - Select all checkboxes from the DOM that share the same combat style as the triggering checkbox.
-  
-//3a. Next, loop over each checkbox with the matching combat style:
-//  - If: the checkbox being looped over is NOT the one that triggered the event,
-//    and the triggering checkbox is checked, then:
-//    - Disable the checkbox currently being looped over.
-//  - Else:
-//    - Do not change its state.
+//2. Attach an event listener to the form element to listen for any change events.
+formElement.addEventListener('change', (e) => {
+    //3. Inside the event listener:
+    //  - Target the checkbox that triggered the change event.
+    //  - Get the combat style of the triggering checkbox from its data-combat-style attribute.
+    //  - Select all checkboxes from the DOM that share the same combat style as the triggering checkbox.
+    const triggeringCheckbox = e.target;
+    const combatStyle = triggeringCheckbox.getAttribute("data-combat-style");
+    const conflictingCombatStyles = document.querySelectorAll(`[data-combat-style="${combatStyle}"]`);
+    //3a. Next, loop over each checkbox with the matching combat style:
+    //  - If: the checkbox being looped over is NOT the one that triggered the event,
+    //    and the triggering checkbox is checked, then:
+    //    - Disable the checkbox currently being looped over.
+    //  - Else:
+    //    - Do not change its state.
+    for (let i = 0; i < conflictingCombatStyles.length; i++) {
+        // if (conflictingCombatStyles[i] !== triggeringCheckbox && triggeringCheckbox.checked) {
+        //     conflictingCombatStyles[i].disabled = true;
+        // } else {
+        //     conflictingCombatStyles[i].disabled = false;
+        // }
+        const conflictingCheckBox = conflictingCombatStyles[i];
+        conflictingCheckBox.disabled = triggeringCheckbox.checked && conflictingCheckBox !== triggeringCheckbox;
+    }
+})
+
+
+
